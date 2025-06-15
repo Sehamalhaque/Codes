6630bd2ae676dc9e37656fc65524684290f3e4cf
Seham@GITS <99060969+Sehamalhaque@users.noreply.github.com>
2025-06-15 18:42:06 +0600

    Time: 63 ms (12.19%) | Memory: 18.2 MB (43.26%) - LeetSync

--- 1203-print-in-order/print-in-order.py
+++ 1203-print-in-order/print-in-order.py
@@ -17,3 +17,22 @@ class Foo:
     def third(self, printThird: 'Callable[[], None]') -> None:
         self.second_done.wait()
         printThird()
+
+def printFirst(): print("first", end="")
+def printSecond(): print("second", end="")
+def printThird(): print("third", end="")
+
+foo = Foo()
+
+from threading import Thread
+
+threads = [
+    Thread(target=foo.third, args=(printThird,)),
+    Thread(target=foo.second, args=(printSecond,)),
+    Thread(target=foo.first, args=(printFirst,))
+]
+
+for t in threads:
+    t.start()
+for t in threads:
+    t.join()
